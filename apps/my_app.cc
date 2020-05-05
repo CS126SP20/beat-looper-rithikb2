e42a9bd223b0d153ed3df94c0ea25af71a18b172
Rithik Booreddy <rithikb2@illinois.edu>
2020-05-05 00:56:24 -0700

    implemented writing to file and track name

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -1,10 +1,12 @@
 // Copyright (c) 2020 [Your Name]. All rights reserved.
 
 #include "my_app.h"
-#include "UI.h"
 
 #include <cinder/app/App.h>
 #include <cinder/audio/Voice.h>
+
+
+#include "UI.h"
 
 using namespace reza::ui;
 using namespace ci;
@@ -18,29 +20,37 @@ MyApp::MyApp() {}
 
 void MyApp::setup() {
   mUi = SuperCanvas::create("beat looper");
-  mUi->addLabel("type filenames and hit enter to map to keys 1-4", FontSize::MEDIUM);
+  mUi->addLabel("type file and track names and hit enter to map to keys 1-4", FontSize::MEDIUM);
   mUi->addSpacer();
   mUi->addSpacer();
 
   for (int i = 1; i < 5; i++) {
-    mUi->
-    addLabel("key " + std::to_string(i) + " filename:", FontSize::MEDIUM);
+    mUi->addLabel("key " + std::to_string(i) + " filename:", FontSize::MEDIUM);
     inputs.push_back(mUi->addTextInput(" ", TextInput::Format()));
     mUi->addSpacer();
     mUi->addSpacer();
   }
+  mUi->addLabel("track name:", FontSize::MEDIUM);
+  textInputRef = mUi->addTextInput(" ", TextInput::Format());
 
   mUi->addLabel("use keys 1-4 to play", FontSize::MEDIUM);
   sliderFormat.precision(0);
   sliderRef = mUi->
       addSliderf("BPM", 60, 20.0, 260, sliderFormat);
-
+  mUi->addLabel("press p to play your loop", FontSize::LARGE);
   mUi->autoSizeToFitSubviews();
 
   ci::audio::SourceFileRef musicFile =
       ci::audio::load(ci::app::loadAsset("metronome.wav"));
   metronome = ci::audio::Voice::create(musicFile);
-
+  /*
+  int rc = sqlite3_open("/Users/school/Library/Preferences/CLion2019.3/consoles/db/8eadde42-230e-451c-83ff-8a982a8afb35", &db_);
+  if (rc == 0) {
+    std::cout << "unable to connect to the database\n";
+  } else {
+    std::cout << "opened database successfully\n";
+  }
+   */
 }
 
 void MyApp::update() {
@@ -59,10 +69,67 @@ void MyApp::play(int key) {
           event(key, (std::chrono::system_clock::now() - start).count()));
     } else {
       isRecording = false;
+      saveEvents();
     }
   }
   mVoice[key - 1]->start();
 }
+
+void MyApp::loadEvents() {
+  std::ifstream readFile = std::ifstream(tracksFolder + (*textInputRef).getValue() + ".txt");
+  std::string token1;
+  int token2;
+  readFile >> token1;
+  readFile >> token2;
+  while (!readFile.end) {
+    readFile >> token1;
+    readFile >> token2;
+    fEvents.push_back(fEvent(token1, token2));
+  }
+}
+
+void MyApp::saveEvents() {
+  fEvents.clear();
+  std::fstream checkFile;
+  checkFile.open(tracksFolder + (*textInputRef).getValue() + ".txt");
+  if (!checkFile.fail()) {
+    loadEvents();
+  }
+  for (event e : events) {
+    fEvents.push_back(fEvent(filenames[e.key - 1], e.timestamp));
+  }
+  std::ofstream trackFile = std::ofstream(tracksFolder + (*textInputRef).getValue() + ".txt");
+  trackFile << "BPM: " << bpm << "\n";
+
+  std::sort(fEvents.begin(), fEvents.end());
+
+  for (fEvent e : fEvents) {
+    trackFile << e.filename << " " << std::to_string(e.timestamp) << "\n";
+  }
+  trackFile.close();
+}
+
+
+/*
+
+int MyApp::callback(void *NotUsed, int argc, char **argv, char **azColName) {
+  return 0;
+}
+
+
+void MyApp::insertEvents() {
+  const char *sql = "INSERT INTO TRACK (name, bpm) VALUES ('test', 70)";
+                    //"VALUES ('" + trackName + "', " + std::to_string(bpm) + ");";
+  char *errMsg = 0;
+  int rc = sqlite3_exec(db_, sql, NULL , 0, &errMsg);
+  if (rc != SQLITE_OK) {
+    std::string sqlErr = "SQL error: ";
+    std::cout <<  sqlErr + errMsg + "\n";
+  } else {
+    std::cout << "inserted into table: track\n";
+  }
+}
+*/
 
 void MyApp::keyDown(KeyEvent event) {
   int key = 0;
--- apps/my_app.h
+++ apps/my_app.h
@@ -5,6 +5,9 @@
 
 #include <cinder/app/App.h>
 #include <cinder/audio/Voice.h>
+#include <sqlite3.h>
+#include <cstdio>
+
 #include "UI.h"
 
 namespace myapp {
@@ -18,14 +21,16 @@ class MyApp : public cinder::app::App {
   void play(int key);
   void keyDown(cinder::app::KeyEvent) override;
   void mouseDown(cinder::app::MouseEvent) override;
-
+  static int callback(void *NotUsed, int argc, char **argv, char **azColName);
   std::string filenames [4];
 
   reza::ui::SuperCanvasRef mUi;
   reza::ui::Sliderf::Format sliderFormat = reza::ui::Sliderf::Format();
   std::vector<reza::ui::TextInputRef> inputs;
+  reza::ui::TextInputRef textInputRef;
   reza::ui::SliderfRef sliderRef;
   int bpm;
+  std::string trackName = "test";
   ci::audio::VoiceRef metronome;
   ci::audio::VoiceRef mVoice [4];
   bool isRecording = false;
@@ -37,8 +42,30 @@ class MyApp : public cinder::app::App {
       key = k;
       timestamp = t;
     }
+    bool operator < (const event& e) const
+    {
+      return (timestamp < e.timestamp);
+    }
   };
+  //void insertEvents();
   std::vector<event> events;
+  void saveEvents();
+  void loadEvents();
+  std::string tracksFolder = "/Users/school/Cinder/my-projects/final-project-rithikb2/assets/tracks/";
+  //sqlite3 *db_;
+  struct fEvent {
+    std::string filename;
+    int timestamp;
+    fEvent(std::string f, int t) {
+      filename = f;
+      timestamp = t;
+    }
+    bool operator < (const fEvent& e) const
+    {
+      return (timestamp < e.timestamp);
+    }
+  };
+  std::vector<fEvent> fEvents;
 };
 
 }  // namespace myapp
